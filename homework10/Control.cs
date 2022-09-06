880f2cc94253f269c38e78f34c8a7af6f93e37dd
user <user@WINDOWS-OM6HICP>
2022-09-06 13:38:15 +0400

    Add project files.

---
+++ homework10/Control.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+
+static class Control
+{
+	public static void MySetColor(ConsoleColor foreground, ConsoleColor background)
+	{
+		Console.ForegroundColor = foreground;
+		Console.BackgroundColor = background;
+	}
+
+	public static int Keyboard(ref int select, int count, bool Esc)
+	{
+		ConsoleKey keyboard = Console.ReadKey().Key;
+
+		switch (keyboard)
+		{
+			case ConsoleKey.UpArrow:
+			case ConsoleKey.LeftArrow:
+			case ConsoleKey.W:
+			case ConsoleKey.A:
+				if (select == 0)
+					select = count;
+				select--;
+				return 1;
+			case ConsoleKey.DownArrow:
+			case ConsoleKey.RightArrow:
+			case ConsoleKey.S:
+			case ConsoleKey.D:
+				select++;
+				select %= count;
+				return 1;
+			case ConsoleKey.Enter:
+				return 0;
+			case ConsoleKey.Escape:
+				if (Esc)
+					return -1;
+				return 1;
+			default:
+				return 1;
+		}
+	}
+
+	public static int GetSelect(string selection, string[] selections, bool Esc = false)
+	{
+		int select = default;
+		int selectionsCount = Convert.ToInt32(selections.Length);
+
+		int isStart = 1;
+		while (isStart > 0)
+		{
+			Console.Clear();
+			Console.WriteLine(selection);
+
+			for (int i = 0; i < selectionsCount; i++)
+			{
+				char arrow = ' ';
+				if (i == select)
+				{
+					MySetColor(ConsoleColor.Blue, ConsoleColor.Red);
+					arrow = ' ';
+				}
+
+				Console.WriteLine($" {arrow} {selections[i]}");
+				Console.ResetColor();
+			}
+
+			if (Esc)
+				Console.WriteLine("\nPress Esc to exit...\n");
+			isStart = Convert.ToInt32(Keyboard(ref select, selectionsCount, Esc));
+
+			if (isStart == -1)
+				return isStart;
+		}
+
+		return select;
+	}
+}
---
+++ homework10/Program.cs
@@ -0,0 +1,57 @@
+﻿class Document
+{
+	public static DocumentProgram? CreateDocument(string type)
+	{
+		return type switch
+		{
+			"DocumentProgram" => new DocumentProgram(),
+			"ProDocumentProgram" => new ProDocumentProgram(),
+			"ExpertDocument" => new ExpertDocument(),
+			_ => null
+		};
+	}
+}
+
+class DocumentProgram
+{
+	public void OpenDocument() => Console.WriteLine("Document Openned");
+	public virtual void EditDocument() => Console.WriteLine("Can Edit in Pro and Expert versions");
+	public virtual void SaveDocument() => Console.WriteLine("Can Save in Pro and Expert versions");
+}
+
+class ProDocumentProgram : DocumentProgram
+{
+	public sealed override void EditDocument() => Console.WriteLine("Document Edited");
+	public sealed override void SaveDocument() => Console.WriteLine("Document Saved in doc format, for pdf format buy Expert packet");
+}
+
+class ExpertDocument : DocumentProgram
+{
+	public sealed override void EditDocument() => Console.WriteLine("Document Edited");
+	public sealed override void SaveDocument() => Console.WriteLine("Document Saved in pdf format");
+}
+
+class Program
+{
+	static void Main()
+	{
+		var answers = new string[] { "Pro Document Program", "Expert Document", "Document Program" };
+
+		while (true)
+		{
+			int choice = (int)Control.GetSelect("\n~ Please, Enter Your Choice :\n", answers, true);
+			
+			if (choice == -1) 
+				break;
+
+			DocumentProgram? dc = Document.CreateDocument(answers[choice].Replace(" ", string.Empty));
+
+			dc?.OpenDocument();
+			dc?.EditDocument();
+			dc?.SaveDocument();
+
+			Console.WriteLine("\nPress any key to continue...");
+			Console.ReadKey();
+		}
+	}
+}
